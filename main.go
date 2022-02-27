835c75c5cbe35451279961fcbd5f25299e8ffbcf
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-27 21:33:44 +0800

    playing

--- main.go
+++ main.go
@@ -2,31 +2,58 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"time"
 )
 
 func main() {
 
 	table := make(chan *ball)
+	done := make(chan *ball)
 
-	go player("bung", table)
-	go player("arif", table)
+	go player("bung", table, done)
+	go player("arif", table, done)
 
-	table <- new(ball)
-	time.Sleep(1 * time.Second)
-	<-table
+	referree(table, done)
 }
 
 type ball struct {
-	hits int
+	hits       int
+	lastPlayer string
 }
 
-func player(name string, table chan *ball) {
+func referree(table chan *ball, done chan *ball) {
+	table <- new(ball)
+
 	for {
-		ball := <-table
-		ball.hits++
-		log.Println(name, "hits the ball", ball.hits)
-		time.Sleep(50 * time.Millisecond)
-		table <- ball
+		select {
+		case ball := <-done:
+			log.Println("winner is", ball.lastPlayer)
+			return
+		}
+	}
+}
+
+func player(name string, table chan *ball, done chan *ball) {
+	for {
+		s := rand.NewSource(time.Now().Unix())
+		r := rand.New(s)
+
+		select {
+		case ball := <-table:
+			v := r.Intn(1000)
+			if v%11 == 0 {
+				log.Println(name, "drop the ball")
+				done <- ball
+				return
+			}
+			ball.hits++
+			ball.lastPlayer = name
+			log.Println(name, "hits the ball", ball.hits)
+			time.Sleep(50 * time.Millisecond)
+			table <- ball
+		case <-time.After(2 * time.Second):
+			return
+		}
 	}
 }
